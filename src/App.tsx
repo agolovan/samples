b1c25363c383effc0afc670ebeab42cebf1ea8c3
Alexander Golovan <a_golovan@hotmail.com>
2024-04-11 20:44:38 -0700

    add NavigationWidget

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import NavigationWidget from './samples/ui/Navigation';
-import {data} from './constants' 
+import { data } from './constants'
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-         <NavigationWidget data={data} />
-         </div>
+        <NavigationWidget data={data} />
       </header>
     </div>
   );
--- src/samples/ui/Navigation.tsx
+++ src/samples/ui/Navigation.tsx
@@ -1,46 +1,36 @@
-//Imagine we want to create a "Navigation Widget" for aws.amazon.com, given the below JSON data, 
+//Create a "Navigation Widget" using JSON data, 
 //create the corresponding markup to render the navigation in a page.
 import React from 'react';
 
 import { IData } from "../../types";
 
+const renderNavItems = (items) => {
+   return Object.keys(items).map((key) => {
+      const subItems = items[key];
+      const hasSubItems = Object.keys(subItems).length > 0;
 
-const displayElement = (data: IData) => {
-   Object.entries(data).forEach(([key, value]) => {
-      console.log(key);
-      if (Object.keys(value).length !== 0) {
-         displayElement(value as IData);
-      }
+      return (
+         <li key={key}>
+            <span>{key}</span>
+            {hasSubItems && (
+               <ul>
+                  {renderNavItems(subItems)}
+               </ul>
+            )}
+         </li>
+      );
    });
-}
+};
 
 const NavigationWidget = (data: IData) => {
-   displayElement(data);
    return (
-      <ul>
-         <li>
-            products
-            <ul>
-               <li>
-                  analytics
-                  <ul>
-                     <li>amazon cloud search</li>
-                  </ul>
-               </li>
-               <li>blockchain</li>
-            </ul>
-         </li>
-         <li>
-            solutions
-            <ul>
-               <li>by use case</li>
-               <li>by industry</li>
-            </ul>
-         </li>
-         <li>pricing</li>
-         <li>documentation</li>
-      </ul>
-   )
+      <div>
+         <h2>Navigation Widget</h2>
+         <ul>
+            {renderNavItems(data)}
+         </ul>
+      </div>
+   );
 }
 
 export default NavigationWidget;
